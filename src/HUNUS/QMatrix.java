5fe553129ed5d366959a07f3fb7a8253ddbfd40b
Felix <123lingfeng321@gmail>
2026-03-01 15:26:02 +0800

    HUNUS

--- src/HUNUS/QMatrix.java
+++ src/HUNUS/QMatrix.java
@@ -36,7 +36,6 @@ public class QMatrix {
 	int[][] matrixItemUtility;
 	/** the qmatrix for remaining utility [item][itemset] -> remaining utility*/
 	int[][] matrixItemRemainingUtility;
-	/** [新] 更紧凑的剩余效用 */
 	int[][] matrixItemTighterRemainingUtility;
 
 	/** the item names (the list of all items in a sequence) 序列中包含的所有item的数组*/
@@ -44,18 +43,15 @@ public class QMatrix {
 	/** the swu of this sequence **/
 	int swu;
 
-	// --- 新增的核心数据结构 ---
-	// 1. 存储标准格式的序列数据，避免重复构建
+
 	private ArrayList<ArrayList<Integer>> sequenceData;
-	// 2. 存储item到其在矩阵中行号的映射，实现O(1)查询
 	private Map<Integer, Integer> itemToIndexMap;
-	// --- 新增结束 ---
 	
 	/**
 	 * Constructor
 	 * @param nbItem the number of item in the sequence
 	 * @param nbItemset the number of itemsets in that sequence
-	 * @param sequence 序列的标准格式数据
+	 * @param sequence
 	 */
 	public QMatrix(int nbItem, int nbItemset, int[] itemNames, int itemNamesLength, int swu,
 				   ArrayList<ArrayList<Integer>> sequence) {
@@ -66,13 +62,11 @@ public class QMatrix {
 		this.itemNames = new int[itemNamesLength];
 		System.arraycopy(itemNames, 0, this.itemNames, 0, itemNamesLength);
 
-		// --- 初始化新增的数据结构 ---
-		this.sequenceData = sequence; // 直接保存，不再重新计算
+		this.sequenceData = sequence;
 		this.itemToIndexMap = new HashMap<>();
 		for (int i = 0; i < itemNamesLength; i++) {
 			this.itemToIndexMap.put(itemNames[i], i);
 		}
-		// --- 初始化结束 ---
 	}
 
 	public int getItemIndex(int item) {
@@ -94,44 +88,6 @@ public class QMatrix {
 		matrixItemRemainingUtility[itemPos][itemset] = remainingUtility;
 	}
 
-	
-	/**
-	 * Get a string representation of this matrix (for debugging purposes)
-	 * @return the string representation
-	 */
-	public String toString() {
-		StringBuffer buffer = new StringBuffer();
-		buffer.append("QMatrix信息:\n");
-		buffer.append("序列效用(SWU): " + swu + "\n");
-		buffer.append("项集数量: " + matrixItemUtility[0].length + "\n");
-		buffer.append("项数量: " + itemNames.length + "\n");
-		buffer.append("项列表: ");
-		for(int i = 0; i < itemNames.length; i++) {
-			buffer.append(itemNames[i]);
-			if(i < itemNames.length - 1) buffer.append(", ");
-		}
-		buffer.append("\n\n");
-		
-		// 打印矩阵表头
-		buffer.append("效用矩阵 (格式: 效用[剩余效用]):\n");
-		buffer.append("项目\\项集\t");
-		for(int j = 0; j < matrixItemUtility[0].length; j++) {
-			buffer.append("项集" + j + "\t");
-		}
-		buffer.append("\n");
-		
-		// 打印矩阵内容
-		for(int i=0; i< itemNames.length; i++) {
-			buffer.append("项" + itemNames[i] + "\t");
-			for(int j=0; j< matrixItemUtility[i].length; j++) {
-				buffer.append(matrixItemUtility[i][j] + "[" + 
-						matrixItemRemainingUtility[i][j] + "]\t");
-			}
-			buffer.append("\n");
-		}
-		buffer.append("\n");
-		return buffer.toString();
-	}
 
 	/**
 	 * Get the number of itemsets in this sequence (Q-Matrix columns).
@@ -141,11 +97,6 @@ public class QMatrix {
 		return matrixItemUtility[0].length;
 	}
 
-	/**
-	 * 获取指定项集（itemset）中的所有 item。
-	 * @param itemsetIdx 项集索引（列号）
-	 * @return 包含该项集所有有效 item 的列表
-	 */
 	public List<Integer> getItemset(int itemsetIdx) {
 		List<Integer> itemset = new ArrayList<>();
 		for (int i = 0; i < itemNames.length; i++) {
@@ -170,38 +121,8 @@ public class QMatrix {
 		return matrixItemRemainingUtility[index][itemset];
 	}
 
-	/**
-	 * [新方法] 获取一个项在给定项集的 "紧凑" 剩余效用。
-	 * @param itemset the itemset
-	 * @param item the item
-	 * @return the tighter remaining utility
-	 */
-	public int getTighterRemainingUtility(int itemset, int item) {
-		// 如果尚未计算，则返回普通剩余效用作为后备
-		if (matrixItemTighterRemainingUtility == null) {
-			return getRemainingUtility(itemset, item);
-		}
-		Integer index = itemToIndexMap.get(item);
-		if (index == null) {
-			return 0;
-		}
-		return matrixItemTighterRemainingUtility[index][itemset];
-	}
 
-	/**
-	 * Get the column count
-	 * @return the column count
-	 */
-	public int getNbColumns() {
-		return matrixItemUtility[0].length;
-	}
 
-	/**
-	 * 高效的方法：
-	 * 只获取指定项集（itemset）中实际存在（效用>0）的 item。
-	 * @param itemsetIdx 项集索引（列号）
-	 * @return 包含该项集所有有效 item 的列表
-	 */
 	 public ArrayList<Integer> getPresentItemsInItemset(int itemsetIdx){
 		 ArrayList<Integer> itemset = new ArrayList<>();
 		 for(int i = 0; i < this.itemNames.length; i++) {
@@ -212,31 +133,18 @@ public class QMatrix {
 		 return itemset;
 	 }
 
-	/**
-	 * 将QMatrix内部数据转换为项集序列，每个项集只包含效用>0的item
-	 * @return 包含所有项集的列表，每个项集是一个包含有效item的列表
-	 * (已优化) 直接返回预存的序列数据
-	 */
+
 	public ArrayList<ArrayList<Integer>> getSequenceAsItemsets(){
 		return this.sequenceData;
 	}
 
-	/**
-	 * [新增] 获取从指定项集索引开始的所有后续项。
-	 * 用于查找 s-extensions。
-	 * @param startColumn 开始搜索的项集索引 (包含此项集)
-	 * @return 一个包含所有不重复项的列表
-	 */
+
 	public List<Integer> getItemsAfter(int startColumn) {
 		List<Integer> items = new ArrayList<>();
-		// 使用一个HashSet来保证项的唯一性，避免重复添加
 		java.util.Set<Integer> seenItems = new java.util.HashSet<>();
 
-		// 从指定的 startColumn 开始遍历到最后一个项集
 		for (int j = startColumn; j < getNbItemsets(); j++) {
-			// 遍历该项集中的所有实际存在的项
 			for (int item : getPresentItemsInItemset(j)) {
-				// 如果这个项还没有被添加过
 				if (seenItems.add(item)) {
 					items.add(item);
 				}
@@ -245,30 +153,21 @@ public class QMatrix {
 		return items;
 	}
 
-	/**
-	 * [新增] 查找一个项在指定项集索引之后（或之中）的首次出现位置。
-	 * 用于 s-extension 的位置检查。
-	 * @param item 要查找的项
-	 * @param startColumn 开始搜索的项集索引 (包含此项集)
-	 * @return 如果找到，返回项集索引；否则返回 -1。
-	 */
+
 	public int getFirstOccurrence(int item, int startColumn) {
 		Integer rowIndex = itemToIndexMap.get(item);
-		// 如果该项根本不存在于此序列的QMatrix中，直接返回-1
 		if (rowIndex == null) {
 			return -1;
 		}
 
-		// 从指定的 startColumn 开始遍历到最后一个项集
+
 		for (int j = startColumn; j < getNbItemsets(); j++) {
-			// 检查该项在当前项集的效用是否大于0
 			if (matrixItemUtility[rowIndex][j] > 0) {
-				// 如果是，说明项存在于此项集，返回当前项集索引
 				return j;
 			}
 		}
 
-		// 如果遍历完所有后续项集都未找到，返回-1
+
 		return -1;
 	}
 
@@ -280,38 +179,29 @@ public class QMatrix {
 		return this.itemToIndexMap;
 	}
 
-	/**
-	 * [新方法] 预计算更紧凑的剩余效用。
-	 * 这个方法只累加那些 "有希望的" 项的效用。
-	 * @param unpromisingItems 一个包含所有 "无望项" 的集合。
-	 */
+
 	public void computeTighterRemainingUtility(Set<Integer> unpromisingItems) {
 		int nbRows = this.itemNames.length;
 		int nbItemsets = getNbItemsets();
         this.matrixItemTighterRemainingUtility = new int[nbRows][nbItemsets];
         int tighterRemainingUtility = 0;
 
-        // 从序列的最后一个项集向前遍历
+
         for (int j = nbItemsets - 1; j >= 0; j--) {
             int currentItemsetUtility = 0;
-            // 遍历当前项集中的所有项
             for (int i = 0; i < nbRows; i++) {
                 int item = this.itemNames[i];
                 int utility = this.matrixItemUtility[i][j];
 
-                // 只有当这个项是 "有希望的" (即不在unpromising集合中) 且效用不为0时，才累加
                 if (utility > 0 && !unpromisingItems.contains(item)) {
                     currentItemsetUtility += utility;
                 }
             }
 
-            // 对于当前项集 j 中的所有项，它们的紧凑剩余效用是相同的，
-            // 即在 j 之后所有 "有希望的" 项的效用之和。
             for (int i = 0; i < nbRows; i++) {
                 this.matrixItemTighterRemainingUtility[i][j] = tighterRemainingUtility;
             }
 
-            // 更新下一次迭代的剩余效用值
             tighterRemainingUtility += currentItemsetUtility;
         }
     }
--- src/MainTestHUNUS.java
+++ src/MainTestHUNUS.java
@@ -6,52 +6,21 @@ import java.io.IOException;
 public class MainTestHUNUS {
 
     public static void main(String[] args) throws IOException {
-        /**设置数据集和阈值**/
-//        String dataset = "test";
 //        String dataset = "MSNBC";
-        String dataset = "test2";
 //        String dataset = "bible";
-//        String dataset = "Sign";
-//        String dataset = "Leviathan";
-//        String dataset = "kosarak";
-//        String dataset = "chess_synthetic_3k";
-//        String dataset = "e_shop_10";
-//        String dataset = "e_shop_1";
-//        String dataset = "e_shop";
+        String dataset = "e_shop";
 //        String dataset = "e_shop_10k";
 //        String dataset = "MicroblogPCU";
 //        String dataset = "OnlineRetail_II_all";
-//        String dataset = "OnlineRetail_II_all_300";
 //        String dataset = "OnlineRetail_II_best";
 
 
         // The algorithm parameters:
         double thresholdratio = 0.055;
-//        double thresholdratio = 0.3;//test
-//        double thresholdratio = 0.030;//MSNBC
-//        double thresholdratio = 0.010;
-//        double thresholdratio = 0.014;//BIBLE
-//        double thresholdratio = 0.045;
-//        double thresholdratio = 0.000230;//相当于bible的0.010
-//        double thresholdratio = 130;//e_shop_10
-//        double thresholdratio = 0.90;//e_shop_1
-//        double thresholdratio = 0.15;//e_shop
-//        double thresholdratio = 0.50;//e_shop_10k
-//        double thresholdratio = 0.91;//MicroblogPCU
-//        double thresholdratio = 0.35;//OnlineRetail_II_all
-//        double thresholdratio = 0.4;//OnlineRetail_II_best
 
         int[][] targetSequence = {
                 {356,-1,10,-1,10,-1,10,-1,-2},//bible
-                {8,-1,17,-1,8,-1,-2},//leviathan
                 {7,-1,6,-1,-2},//msnbc
-                {1857,4250,-1,-2},//syn
-                {3,-1,5,-1,7,-1,-2},//chess_synthetic_3k [3,5] [3,5,7] [3,5,7,40]
-                {8,-1,9,-1,-2},//sign
-                {11,-1,218,-1,6,-1,148,-1,-2},//kosarak 10k
-                {1,-1,3,5,-1,3,-1,-2},//test
-                {1,-1,3,-1,-2},//test2
-                {1,-1,-2},//test3
                 {1,132,192,-1,28,212,-1,-2},//e_shop
                 {12766,-1,39356,-1,-2},//MicroblogPCU_two_items
                 {39356},//MicroblogPCU_one_item
@@ -68,18 +37,18 @@ public class MainTestHUNUS {
 
 
         int minlen = 1;
-        int maxlen = 5;
+        int maxlen = 20;
         algo.setMinLen(minlen);
         algo.setMaxLen(maxlen);
 
 
         int mingap = 0;
-        int maxgap = 3;
+        int maxgap = 5;
         algo.setMinGap(mingap);
         algo.setMaxGap(maxgap);
 
         //set target sequence
-        algo.setTargetSequence(targetSequence[8]);
+        algo.setTargetSequence(targetSequence[2]);
 
 
         //the path for saving the patterns found
@@ -87,10 +56,6 @@ public class MainTestHUNUS {
                 + "_len" + minlen + '-' + maxlen
                 + "_gap" + mingap + '-' + maxgap + ".txt";
         System.out.println(output);
-//        String  output = "output/HUNUS/" + "HUNUS_" + dataset + '_' + thresholdratio
-//                + "_len" + minlen + '-' + maxlen
-//                + "_gap" + mingap + '-' + maxgap + "complex" + ".txt";
-//        System.out.println(output);
 
         algo.runAlgoHUNUS(input, output, thresholdratio, dataset);
     }
